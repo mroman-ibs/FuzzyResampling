3a8cbc4d02cd4ce14dba609c28c9a9dafd7a1312
Maciej Romaniuk <mroman@ibspan.waw.pl>
2021-09-24 07:32:44 +0200

    Polishing general help for package

--- R/FuzzyResampling-package.R
+++ R/FuzzyResampling-package.R
@@ -1,3 +1,15 @@
+#' @section  Resampling procedures:
+#' The following resampling procedures are available in the package:
+#'
+#' + *classicalBootstrap* - classical approach based on Efron's method,
+#' + *VAmethod* - resampling method which preserves the value and ambiguity,
+#' + *EWmethod* - resampling method which preserves the expected value and width,
+#' + *VAAmethod* - resampling method which preserves the value, left-hand and right-hand ambiguities,
+#' + *VAFmethod* - resampling method which preserves the value, ambiguity and fuzziness,
+#' + *dmethod* - resampling method which preserves the left end of the cores and increments,
+#' + *wmethod* - resampling method which uses the special *w density* to "smooth" the output fuzzy value.
+
+
 #' @keywords internal
 "_PACKAGE"
 
@@ -5,4 +17,8 @@
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
 ## usethis namespace: end
+
+
+
+
 NULL
